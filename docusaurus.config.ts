ea8907a7755d30a9cbffda4b168c421d32a71b64
Aethelred-The-Ready <alic.yognout@gmail.com>
2026-02-10 20:45:11 -0400

    Change case

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,7 +22,7 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Aethelred-The-Ready', // Usually your GitHub org/user name.
+  organizationName: 'aethelred-the-ready', // Usually your GitHub org/user name.
   projectName: 'crucible-wiki', // Usually your repo name.
 
   onBrokenLinks: 'throw',
